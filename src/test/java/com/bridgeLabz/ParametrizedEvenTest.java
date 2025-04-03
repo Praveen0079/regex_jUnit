36266e5f05abcae1532f5e08fc366d8054ec4de2
Praveen Sharma <96952903+Praveen0079@users.noreply.github.com>
2025-04-03 23:22:59 +0530

    [Praveen] ADD : Parametrized Even Test

--- src/test/java/com/bridgeLabz/ParametrizedEvenTest.java
+++ src/test/java/com/bridgeLabz/ParametrizedEvenTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class ParametrizedEvenTest {
-
+ 
 
     private NumberUtils obj=new NumberUtils();
 
